22b43a0714f7cfd054ae304c24f9617ede299814
Maksus517 <maksim517@mail.ru>
2023-05-11 10:45:32 +0300

    Change joke

--- services/joke.py
+++ services/joke.py
@@ -1,18 +1,24 @@
-import requests
 from bs4 import BeautifulSoup as BS
 import random
 import asyncio
+import aiohttp
 
 
 URL = 'https://www.anekdot.ru/last/good/'
 
 
-def joke_pars(url=URL) -> list:
-    get = requests.get(url)
-    soup = BS(get.text, 'html.parser')
-    jokes = soup.find_all('div', class_='text')
-    return [i.text for i in jokes]
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
 
-joke = joke_pars()
+async def joke_pars(url):
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, url)
+        soup = BS(html, 'html.parser')
+        jokes = soup.find_all('div', class_='text')
+        return [i.text for i in jokes]
+
+loop = asyncio.get_event_loop()
+joke = loop.run_until_complete(joke_pars(URL))
 random.shuffle(joke)
--- services/news.py
+++ services/news.py
@@ -1,14 +1,27 @@
-import requests
-
 from bs4 import BeautifulSoup as BS
 import asyncio
+import aiohttp
+
 
 URL = 'https://ria.ru/world/'
 
 
-def news_parser(url: str = URL):
-    page = requests.get(url)
-    soup = BS(page.content, 'html.parser')
-    post = soup.find_all("a", class_="list-item__title color-font-hover-only")[0:15]
-    for i in range(len(post)):
-        yield f'{post[i]["href"]}\n\n'
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.read()
+
+
+async def news_parser(url: str) -> list:
+    async with aiohttp.ClientSession() as session:
+        html = await fetch(session, url)
+        soup = BS(html, 'html.parser')
+        post = soup.find_all("a", class_="list-item__title color-font-hover-only")[0:15]
+        news_list = []
+        for i in range(len(post)):
+            news_list.append(f'{post[i]["href"]}\n\n')
+        return news_list
+
+
+loop = asyncio.get_event_loop()
+news = loop.run_until_complete(news_parser(URL))
+
