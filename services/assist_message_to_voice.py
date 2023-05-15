1ecaa64aa75b51081aefcf8eb55463ab7a14a7bf
Maksus517 <maksim517@mail.ru>
2023-05-15 15:50:10 +0300

    add support_project

--- services/assist_message_to_voice.py
+++ services/assist_message_to_voice.py
@@ -5,9 +5,3 @@ from aiogram.types import Message
 async def message_to_mp3(message: Message) -> None:
     tts = gTTS(message.text, lang='ru')
     tts.save(f'voice from {message.from_user.id}.mp3')
-
-
-async def message_to_voice(message: str) -> list:
-    tts = gTTS(message, lang='ru')
-    a = tts.get_bodies()
-    return a
